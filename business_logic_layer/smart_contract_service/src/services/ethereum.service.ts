2f79a145a21d6695d7e208146818583599c78d31
ChamHerman <hermancham08@gmail.com>
2026-08-22 10:39:10 +0800

    fix: dotenv

--- business_logic_layer/smart_contract_service/src/services/ethereum.service.ts
+++ business_logic_layer/smart_contract_service/src/services/ethereum.service.ts
@@ -1,6 +1,8 @@
 import { ethers } from "ethers";
 import * as path from "path";
 import * as fs from "fs";
+import * as dotenv from "dotenv";
+
 const envPaths = [
   path.resolve(__dirname, "../../../../../.env"),
   path.resolve(__dirname, "../../../../.env"),
